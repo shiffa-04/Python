fff280b492537235c80b53b97c132a4d37198eb8
shiffa-04 <shiffatariq@gmail.com>
2024-09-23 19:45:38 +0530

    Implement birthdayCakeCandles function to count tallest candles

---
+++ birthdayCandles.py
@@ -0,0 +1,36 @@
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'birthdayCakeCandles' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY candles as parameter.
+#
+
+def birthdayCakeCandles(candles):
+    # Write your code here
+    count_occ = 0
+    max_height = max(candles)
+    for i in candles:
+        if i ==max_height:
+            count_occ += 1
+    return count_occ
+    
+    
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
