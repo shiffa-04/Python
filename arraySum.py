948042d946a23d965b8e449785fd28d5384e8a15
shiffa-04 <shiffatariq@gmail.com>
2024-09-28 22:00:19 +0530

    Add sum the elements in array

---
+++ arraySum.py
@@ -0,0 +1,20 @@
+def simpleArraySum(ar):
+    # Write your code here
+    sum = 0
+    for i in ar:
+        sum += i
+    return sum
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = simpleArraySum(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
