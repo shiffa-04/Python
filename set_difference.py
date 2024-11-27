f5cdd558e71063fdee9819cf1b98eda7997309f6
shiffa-04 <shiffatariq@gmail.com>
2024-11-27 23:05:43 +0530

    Add script to calculate students subscribed only to English newspapers

---
+++ set_difference.py
@@ -0,0 +1,7 @@
+stu_eng = int(input())
+eng_students = set(input().split())
+stu_fre = int(input())
+fre_students = set(input().split())
+
+subscribed_both = eng_students.difference(fre_students)
+print(len(subscribed_both))
